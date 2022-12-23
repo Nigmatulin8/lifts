1980f01319a24fabf09294c224792361fbb0cdf8
Nigmatulin <rustam@ikiteo.com>
2022-12-23 21:10:15 +0300

    Step 4. Адекватные движения лифта

---
+++ src/components/button.vue
@@ -0,0 +1,55 @@
+<template>
+    <div
+        class="button"
+        :style="{ 'height': `calc(100% / ${floors})` }"
+    >
+        <button @click="callLift(floors - floor + 1)">
+            {{ floors - floor + 1  }}
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+
+        props: {
+            // Все этажи
+            floors: {
+                type: Number,
+                required: true,
+            },
+
+            // Этаж, к которой принажлежит кнопка
+            floor: {
+                type: Number,
+                required: true,
+            },
+        },
+
+        methods: {
+            callLift(floor) {
+                this.$emit('click', floor);
+            },
+        }
+    }
+</script>
+
+<style lang="scss">
+    .button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        button {
+            width: 28px;
+            height: 28px;
+            font-family: 'Roboto';
+            background: none;
+            border-radius: 50%;
+            border: none;
+            outline: 2px solid #1e78b7;
+            cursor: pointer;
+        }
+    }
+</style>
